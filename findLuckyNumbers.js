617bb1e91230b1f77b3d2f2e411daff6eaf4dac8
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-04-26 20:13:25 -0600

    Initial commit/Solved challenge

---
+++ findLuckyNumbers.js
@@ -0,0 +1,45 @@
+// Write your code below this line.
+
+//create sorted array with vals from 1 through max random "lucky" number desired
+const nums = [];
+const MAX_RANDOM_NUMBER = 10;
+for(let i = 0; i < MAX_RANDOM_NUMBER; ++i) {
+    nums.push(i + 1);
+}
+
+//generate lucky nums with no repeats unless numPicks is greater than MAX_RANDOM_NUM
+//each number generates in constant time
+//algorithm performs in O(numPicks)
+//no need to traverse answer array to check previous picks
+function generateLuckyNums(numPicks) {
+    //set variable to final index of array
+    let maxIndex = nums.length - 1;
+    let answer = [];
+
+    //for every pick
+    //-if the maxIndex var is less than zero, reset it (number of picks is bigger than MAX_RANDOM_NUM)
+    //-pick a random number between 0 and maxIndex
+    //-swap the value of the nums array at that random number with the value of the nums array at the maxIndex
+    //-add that value to the answer array
+    //-decrement max index
+    for(let i = 0; i < numPicks; ++i) {
+        if(maxIndex < 0) {
+            maxIndex = nums.length - 1;
+        }
+
+        let pickedIndex = Math.floor(Math.random() * (maxIndex + 1));
+        let temp = nums[maxIndex];
+        nums[maxIndex] = nums[pickedIndex];
+        nums[pickedIndex] = temp;
+        answer.push(nums[maxIndex]);
+        maxIndex--;
+    }
+
+    return answer;
+}
+
+console.log(generateLuckyNums(4));
+console.log(generateLuckyNums(4));
+console.log(generateLuckyNums(7));
+console.log(generateLuckyNums(7));
+console.log(generateLuckyNums(9));
